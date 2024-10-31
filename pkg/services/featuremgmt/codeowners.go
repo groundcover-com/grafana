9b784806903aac6f8fe2465d230d8dc66c1f658b
Erez Levi <erez@prodstack.dev>
2024-10-31 13:05:28 +0000

    codeowner

--- pkg/services/featuremgmt/codeowners.go
+++ pkg/services/featuremgmt/codeowners.go
@@ -28,5 +28,5 @@ const (
 	enterpriseDatasourcesSquad                  codeowner = "@grafana/enterprise-datasources"
 	grafanaSharingSquad                         codeowner = "@grafana/sharing-squad"
 	grafanaDatabasesFrontend                    codeowner = "@grafana/databases-frontend"
-	groundcover                                 codeowner = "@groundcover"
+	groundcover                                 codeowner = "@groundcover/groundcover"
 )
